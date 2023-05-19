55806c0b3cf8f16385a6ea7a0bf836fee6aa7c14
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-05-18 22:33:53 -0300

    Update FavoritosCuidadorDTO.cs

--- API_CUIDADORES/API_CUIDADORES/DTO/FavoritosCuidadorDTO.cs
+++ API_CUIDADORES/API_CUIDADORES/DTO/FavoritosCuidadorDTO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +11,8 @@ namespace API_CUIDADORES.DTO
         public int id { get; set; }
         public int usuario_id { get; set; }
         public int cuidador_id { get; set; }
+
+        // informações do cuidador
+        public UsuariosDTO usuario { get; set; }
     }
 }
