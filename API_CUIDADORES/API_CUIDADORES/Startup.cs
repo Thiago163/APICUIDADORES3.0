fadff709f72cedd7127b1f73e02de33250c6dde0
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-01 20:13:59 -0300

    Update Startup.cs

--- API_CUIDADORES/API_CUIDADORES/Startup.cs
+++ API_CUIDADORES/API_CUIDADORES/Startup.cs
@@ -1,4 +1,5 @@
 using API_CUIDADORES.DAO;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,9 +8,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace API_CUIDADORES
@@ -26,11 +29,29 @@ namespace API_CUIDADORES
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            // Adicione suas dependências aqui, se necessário
-            services.AddSingleton<UsuariosDAO>();
-            services.AddSingleton<CuidadoresDAO>();
+            var secretKey = "16b564d8e7b9b32a36edf518df48e40c35aeb933ce4abab8e75fd3e5ef89a7f6";
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = false,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true, // Habilitar a validação de expiração do token
+                    ClockSkew = TimeSpan.Zero // Definir a margem de tempo zero para a validação do tempo de expiração
+                };
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +70,8 @@ namespace API_CUIDADORES
             .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader());
+
+            app.UseAuthentication();
 
 
             app.UseHttpsRedirection();
