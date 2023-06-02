1b4d93e7435b8278c93251a58829066ceb0c07dc
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-01 21:57:02 -0300

    cadusu

---
+++ API_CUIDADORES/API_CUIDADORES/Controllers/CadastrousuController.cs
@@ -0,0 +1,24 @@
+﻿using API_CUIDADORES.DAO;
+using API_CUIDADORES.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_CUIDADORES.Controllers
+{
+    namespace API_CUIDADORES.Controllers
+    {
+        [Route("api/cadusu")]
+        [ApiController]
+
+        public class CadastrousuController : ControllerBase
+        {
+
+            [HttpPost]
+            public IActionResult Cadastrar(CuidadoresDTO cuidador)
+            {
+                CuidadoresDAO dao = new CuidadoresDAO();
+                dao.Cadastrar(cuidador);
+                return Ok();
+            }
+        }
+    }
+}
