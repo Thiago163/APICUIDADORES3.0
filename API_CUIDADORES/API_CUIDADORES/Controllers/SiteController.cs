8c649c9589e78227ae5293aa07065eb55e0b9213
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-03 18:36:28 -0300

    site

---
+++ API_CUIDADORES/API_CUIDADORES/Controllers/SiteController.cs
@@ -0,0 +1,22 @@
+﻿using API_CUIDADORES.DAO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_CUIDADORES.Controllers
+{
+    [Route("api/site")]
+    [ApiController]
+    public class SiteController : Controller
+    {
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            CuidadoresDAO dao = new CuidadoresDAO();
+            var cuidador = dao.Listar();
+            return Ok(cuidador);
+        }
+    }
+}
