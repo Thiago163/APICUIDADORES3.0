fa38dde338ace517b0f8126109a0a1169e3d5950
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-03 21:31:12 -0300

    Add files via upload

---
+++ API_CUIDADORES/API_CUIDADORES/DTO/RecentesCuidadorDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_CUIDADORES.DTO
+{
+    public class RecentesCuidadorDTO
+    {
+        // tabela recentes
+        public int id { get; set; }
+        public int usuario_id { get; set; }
+        public int cuidador_id { get; set; }
+
+        // informações do cuidador
+        public UsuariosDTO usuario { get; set; }
+    }
+}
---
+++ API_CUIDADORES/API_CUIDADORES/DTO/RecentesUsuarioDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_CUIDADORES.DTO
+{
+    public class RecentesUsuarioDTO
+    {
+        // tabela recentes
+        public int id { get; set; }
+        public int usuario_id { get; set; }
+        public int cuidador_id { get; set; }
+
+        // informações do cuidador
+        public CuidadoresDTO cuidador { get; set; }
+    }
+}
