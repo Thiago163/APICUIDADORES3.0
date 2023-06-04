9d2184559d5f85e5e681dbb5c1578e21d5e14e58
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-03 22:26:58 -0300

    Add files via upload

---
+++ API_CUIDADORES/API_CUIDADORES/Controllers/RecentesCuidadorController.cs
@@ -0,0 +1,57 @@
+﻿using API_CUIDADORES.DAO;
+using API_CUIDADORES.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace API_CUIDADORES.Controllers
+{
+    [ApiController]
+    [Route("api/recentescui")]
+    [Authorize]
+
+    public class RecentesCuidadorController : ControllerBase
+    {
+        private RecentesCuidadorDAO RecentesCuidadorDAO;
+
+        public RecentesCuidadorController()
+        {
+            RecentesCuidadorDAO = new RecentesCuidadorDAO();
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            var recentes = RecentesCuidadorDAO.Listar();
+            return Ok(recentes);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody] RecentesCuidadorDTO recente)
+        {
+            try
+            {
+                RecentesCuidadorDAO.Cadastrar(recente);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{usuario_id}/{cuidador_id}")]
+        public IActionResult Remover(int usuario_id, int cuidador_id)
+        {
+            RecentesCuidadorDAO.Remover(usuario_id, cuidador_id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Alterar([FromBody] RecentesCuidadorDTO recente)
+        {
+            RecentesCuidadorDAO.Alterar(recente);
+            return Ok();
+        }
+    }
+}
---
+++ API_CUIDADORES/API_CUIDADORES/Controllers/RecentesUsuarioController.cs
@@ -0,0 +1,57 @@
+﻿using API_CUIDADORES.DAO;
+using API_CUIDADORES.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace API_CUIDADORES.Controllers
+{
+    [ApiController]
+    [Route("api/recentesusu")]
+    [Authorize]
+
+    public class RecentesUsuarioController : ControllerBase
+    {
+        private RecentesUsuarioDAO RecentesUsuarioDAO;
+
+        public RecentesUsuarioController()
+        {
+            RecentesUsuarioDAO = new RecentesUsuarioDAO();
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            var recentes = RecentesUsuarioDAO.Listar();
+            return Ok(recentes);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromBody] RecentesUsuarioDTO recente)
+        {
+            try
+            {
+                RecentesUsuarioDAO.Cadastrar(recente);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{usuario_id}/{cuidador_id}")]
+        public IActionResult Remover(int usuario_id, int cuidador_id)
+        {
+            RecentesUsuarioDAO.Remover(usuario_id, cuidador_id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Alterar([FromBody] RecentesUsuarioDTO recente)
+        {
+            RecentesUsuarioDAO.Alterar(recente);
+            return Ok();
+        }
+    }
+}
