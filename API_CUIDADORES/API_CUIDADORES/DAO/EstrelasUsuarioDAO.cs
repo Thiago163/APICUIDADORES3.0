058ab0ad313c0dd8070da0a78f4c7862d50af041
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-11 12:42:10 -0300

    Update EstrelasUsuarioDAO.cs

--- API_CUIDADORES/API_CUIDADORES/DAO/EstrelasUsuarioDAO.cs
+++ API_CUIDADORES/API_CUIDADORES/DAO/EstrelasUsuarioDAO.cs
@@ -1,9 +1,7 @@
-﻿using API_CUIDADORES.DTO;
+using API_CUIDADORES.DTO;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace API_CUIDADORES.DAO
 {
@@ -11,85 +9,85 @@ namespace API_CUIDADORES.DAO
     {
         public List<EstrelasUsuarioDTO> Listar()
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
-
-            var query = "SELECT * FROM estrelasusuario";
-
-            var comando = new MySqlCommand(query, conexao);
-            var dataReader = comando.ExecuteReader();
-
-            var estrelas = new List<EstrelasUsuarioDTO>();
-
-            while (dataReader.Read())
+            using (var conexao = ConnectionFactory.Build())
             {
-                var estrela = new EstrelasUsuarioDTO();
-                estrela.id = Convert.ToInt32(dataReader["id"]);
-                estrela.usuario_id = Convert.ToInt32(dataReader["usuario_id"]);
-                estrela.cuidador_id = Convert.ToInt32(dataReader["cuidador_id"]);
-                estrela.estrela = Convert.ToInt32(dataReader["estrela"]);
+                conexao.Open();
 
-                estrelas.Add(estrela);
+                var query = "SELECT * FROM estrelasusuario";
+
+                var comando = new MySqlCommand(query, conexao);
+                var dataReader = comando.ExecuteReader();
+
+                var estrelas = new List<EstrelasUsuarioDTO>();
+
+                while (dataReader.Read())
+                {
+                    var estrela = new EstrelasUsuarioDTO();
+                    estrela.id = Convert.ToInt32(dataReader["id"]);
+                    estrela.usuario_id = Convert.ToInt32(dataReader["usuario_id"]);
+                    estrela.cuidador_id = Convert.ToInt32(dataReader["cuidador_id"]);
+                    estrela.estrela = Convert.ToInt32(dataReader["estrela"]);
+
+                    estrelas.Add(estrela);
+                }
+
+                return estrelas;
             }
-
-            conexao.Close();
-
-            return estrelas;
         }
 
         public void Cadastrar(EstrelasUsuarioDTO estrela)
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
 
-            var query = @"INSERT INTO estrelasusuario (usuario_id, cuidador_id, estrela)
-                          VALUES (@usuario_id, @cuidador_id, @estrela)";
+                var query = @"INSERT INTO estrelasusuario (usuario_id, cuidador_id, estrela)
+                              VALUES (@usuario_id, @cuidador_id, @estrela)";
 
-            var comando = new MySqlCommand(query, conexao);
-            comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
-            comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
-            comando.Parameters.AddWithValue("@estrela", estrela.estrela);
+                var comando = new MySqlCommand(query, conexao);
+                comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
+                comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
+                comando.Parameters.AddWithValue("@estrela", estrela.estrela);
 
-            comando.ExecuteNonQuery();
-
-            conexao.Close();
+                comando.ExecuteNonQuery();
+            }
         }
 
         public void Remover(int id)
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
 
-            var query = @"Delete from estrelasusuario where id=@id";
+                var query = @"DELETE FROM estrelasusuario WHERE id = @id";
 
-            var comando = new MySqlCommand(query, conexao);
-            comando.Parameters.AddWithValue("@id", id);
+                var comando = new MySqlCommand(query, conexao);
+                comando.Parameters.AddWithValue("@id", id);
 
-            comando.ExecuteNonQuery();
-            conexao.Close();
-
+                comando.ExecuteNonQuery();
+            }
         }
 
         public void Alterar(EstrelasUsuarioDTO estrela)
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
 
-            var query = @"UPDATE estrelasusuario SET 
-                        usuario_id = @usuario_id,
-                        cuidador_id = @cuidador_id,
-                        estrela = @estrela
-                        WHERE id = @id";
+                var query = @"UPDATE estrelasusuario SET 
+                            usuario_id = @usuario_id,
+                            cuidador_id = @cuidador_id,
+                            estrela = @estrela
+                            WHERE id = @id";
 
-            var comando = new MySqlCommand(query, conexao);
-            comando.Parameters.AddWithValue("@id", estrela.id);
-            comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
-            comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
-            comando.Parameters.AddWithValue("@estrela", estrela.estrela);
+                var comando = new MySqlCommand(query, conexao);
+                comando.Parameters.AddWithValue("@id", estrela.id);
+                comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
+                comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
+                comando.Parameters.AddWithValue("@estrela", estrela.estrela);
 
-            comando.ExecuteNonQuery();
-
-            conexao.Close();
+                comando.ExecuteNonQuery();
+            }
         }
     }
 }
