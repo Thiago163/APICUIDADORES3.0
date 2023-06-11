76c03ff742e8737645d7e4473e2437ad72d55677
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-11 12:32:32 -0300

    Update EstrelasCuidadorDAO.cs

--- API_CUIDADORES/API_CUIDADORES/DAO/EstrelasCuidadorDAO.cs
+++ API_CUIDADORES/API_CUIDADORES/DAO/EstrelasCuidadorDAO.cs
@@ -11,85 +11,102 @@ namespace API_CUIDADORES.DAO
     {
         public List<EstrelasCuidadorDTO> Listar()
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
-
-            var query = "SELECT * FROM estrelascuidador";
-
-            var comando = new MySqlCommand(query, conexao);
-            var dataReader = comando.ExecuteReader();
-
-            var estrelas = new List<EstrelasCuidadorDTO>();
-
-            while (dataReader.Read())
+            using (var conexao = ConnectionFactory.Build())
             {
-                var estrela = new EstrelasCuidadorDTO();
-                estrela.id = Convert.ToInt32(dataReader["id"]);
-                estrela.usuario_id = Convert.ToInt32(dataReader["usuario_id"]);
-                estrela.cuidador_id = Convert.ToInt32(dataReader["cuidador_id"]);
-                estrela.estrela = Convert.ToInt32(dataReader["estrela"]);
+                conexao.Open();
 
-                estrelas.Add(estrela);
+                var query = "SELECT * FROM estrelascuidador";
+
+                var comando = new MySqlCommand(query, conexao);
+                var dataReader = comando.ExecuteReader();
+
+                var estrelas = new List<EstrelasCuidadorDTO>();
+
+                while (dataReader.Read())
+                {
+                    var estrela = new EstrelasCuidadorDTO();
+                    estrela.id = Convert.ToInt32(dataReader["id"]);
+                    estrela.usuario_id = Convert.ToInt32(dataReader["usuario_id"]);
+                    estrela.cuidador_id = Convert.ToInt32(dataReader["cuidador_id"]);
+                    estrela.estrela = Convert.ToInt32(dataReader["estrela"]);
+
+                    estrelas.Add(estrela);
+                }
+
+                return estrelas;
             }
-
-            conexao.Close();
-
-            return estrelas;
         }
 
         public void Cadastrar(EstrelasCuidadorDTO estrela)
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
 
-            var query = @"INSERT INTO estrelascuidador (usuario_id, cuidador_id, estrela)
-                          VALUES (@usuario_id, @cuidador_id, @estrela)";
+                var query = @"INSERT INTO estrelascuidador (usuario_id, cuidador_id, estrela)
+                              VALUES (@usuario_id, @cuidador_id, @estrela)";
 
-            var comando = new MySqlCommand(query, conexao);
-            comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
-            comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
-            comando.Parameters.AddWithValue("@estrela", estrela.estrela);
+                var comando = new MySqlCommand(query, conexao);
+                comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
+                comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
+                comando.Parameters.AddWithValue("@estrela", estrela.estrela);
 
-            comando.ExecuteNonQuery();
-
-            conexao.Close();
+                comando.ExecuteNonQuery();
+            }
         }
 
         public void Remover(int id)
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
 
-            var query = @"Delete from estrelascuidador where id=@id";
+                var query = @"DELETE FROM estrelascuidador WHERE id = @id";
 
-            var comando = new MySqlCommand(query, conexao);
-            comando.Parameters.AddWithValue("@id", id);
+                var comando = new MySqlCommand(query, conexao);
+                comando.Parameters.AddWithValue("@id", id);
 
-            comando.ExecuteNonQuery();
-            conexao.Close();
-
+                comando.ExecuteNonQuery();
+            }
         }
 
         public void Alterar(EstrelasCuidadorDTO estrela)
         {
-            var conexao = ConnectionFactory.Build();
-            conexao.Open();
+            using (var conexao = ConnectionFactory.Build())
+            {
+                conexao.Open();
 
-            var query = @"UPDATE estrelascuidador SET 
-                        usuario_id = @usuario_id,
-                        cuidador_id = @cuidador_id,
-                        estrela = @estrela
-                        WHERE id = @id";
+                var query = @"UPDATE estrelascuidador SET 
+                            usuario_id = @usuario_id,
+                            cuidador_id = @cuidador_id,
+                            estrela = @estrela
+                            WHERE id = @id";
 
-            var comando = new MySqlCommand(query, conexao);
-            comando.Parameters.AddWithValue("@id", estrela.id);
-            comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
-            comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
-            comando.Parameters.AddWithValue("@estrela", estrela.estrela);
+                var comando = new MySqlCommand(query, conexao);
+                comando.Parameters.AddWithValue("@id", estrela.id);
+                comando.Parameters.AddWithValue("@usuario_id", estrela.usuario_id);
+                comando.Parameters.AddWithValue("@cuidador_id", estrela.cuidador_id);
+                comando.Parameters.AddWithValue("@estrela", estrela.estrela);
 
-            comando.ExecuteNonQuery();
+                comando.ExecuteNonQuery();
+            }
+        }
 
-            conexao.Close();
+        public void AdicionarEstrela(int cuidadorId, int estrela)
+        {
+            var estrelaDTO = new EstrelasCuidadorDTO()
+            {
+                usuario_id = 0, // Define o valor adequado para o usuário
+                cuidador_id = cuidadorId,
+                estrela = estrela
+            };
+
+            Cadastrar(estrelaDTO);
+        }
+
+        public void RemoverEstrela(int id)
+        {
+            Remover(id);
         }
     }
 }
