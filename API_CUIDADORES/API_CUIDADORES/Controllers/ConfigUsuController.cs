4a94d3c1db429ccda32f7e41906b5aee29c8a1d3
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-22 19:40:50 -0300

    ConfigUsuController.cs

---
+++ API_CUIDADORES/API_CUIDADORES/Controllers/ConfigUsuController.cs
@@ -0,0 +1,45 @@
+﻿using API_CUIDADORES.DAO;
+using API_CUIDADORES.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace API_CUIDADORES.Controllers
+{
+    [ApiController]
+    [Route("api/configusu")]
+    public class ConfigUsuController : ControllerBase
+    {
+        private readonly ConfigUsuDAO _configUsuDAO;
+
+        public ConfigUsuController()
+        {
+            _configUsuDAO = new ConfigUsuDAO();
+        }
+
+        [HttpGet]
+        public ActionResult<List<UsuariosDTO>> Listar()
+        {
+            var usuarios = _configUsuDAO.Listar();
+            return Ok(usuarios);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Remover(int id)
+        {
+            _configUsuDAO.Remover(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Alterar(int id, [FromBody] UsuariosDTO usuario)
+        {
+            if (id != usuario.id)
+            {
+                return BadRequest();
+            }
+
+            _configUsuDAO.Alterar(usuario);
+            return NoContent();
+        }
+    }
+}
