271bcb7b0dc35495fb1beee18b96253d1b837d15
Thiago_Cruz <101190863+Thiago163@users.noreply.github.com>
2023-06-24 14:11:37 -0300

    Update ConfigUsuDAO.cs

--- API_CUIDADORES/API_CUIDADORES/DAO/ConfigUsuDAO.cs
+++ API_CUIDADORES/API_CUIDADORES/DAO/ConfigUsuDAO.cs
@@ -94,7 +94,7 @@ namespace API_CUIDADORES.DAO
                 nome = IFNULL(@nome, nome),
                 sobrenome = IFNULL(@sobrenome, sobrenome),
                 cidade = IFNULL(@cidade, cidade),
-                sexos_id = IFNULL(@sexos_id, 4),
+                sexos_id = IFNULL(@sexos_id, sexos_id),
                 estado = IFNULL(@estado, estado),
                 data_de_nasc = IFNULL(@data_de_nasc, data_de_nasc),
                 cpf = IFNULL(@cpf, cpf),
